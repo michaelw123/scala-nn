486cfad62670056ccdffa163c2a0a0580181389f
michaelw123 <ok1bridge>
2018-06-26 14:05:27 -0400

    initial checkin

--- src/test/scala/net/scala/nn/test/Mnist.scala
+++ src/test/scala/net/scala/nn/test/Mnist.scala
@@ -1,8 +1,13 @@
 package net.scala.nn.test
 
-import java.io.File
-import java.net.URL
+import scala.collection.mutable.ListBuffer
+import java.io.DataInputStream
+
 import java.nio.file.Files
+
+import breeze.linalg.{DenseVector, DenseMatrix}
+
+import scala.io.Source
 
 /**
   * Created by wangmich on 06/26/2018.
@@ -13,6 +18,41 @@ object Mnist extends App {
                     "test" -> "train-images-idx3-ubyte",
                     "test-label" -> "train-labels-idx1-ubyte")
 
-  println(new File("t10k-images-idx3-ubyte").exists())
+  val stream = getClass.getResourceAsStream("/t10k-labels-idx1-ubyte")
+  val dataStream  = new DataInputStream(stream)
+  val labels = ListBuffer.empty[DenseVector[Double]]
 
+  println(dataStream.readInt)
+  val count = dataStream.readInt()
+  println(count)
+
+  for (c <- 0 until count) {
+    val label = dataStream.readByte()
+    labels += DenseVector.tabulate[Double](10)({ i => if (i == label) 1.0 else 0.0 })
+  }
+  println(labels.toList)
+  dataStream.close()
+
+  val imageStream = getClass.getResourceAsStream("/t10k-images-idx3-ubyte")
+  val imageDataStream  = new DataInputStream(imageStream)
+  val images = ListBuffer.empty[DenseVector[Double]]
+  println(imageDataStream.readInt())
+
+  val imageCount = imageDataStream.readInt()
+  val height = imageDataStream.readInt()
+  val width = imageDataStream.readInt()
+
+  println(s"imageCount=${imageCount}, height=${height}, width=${width}")
+
+  for (c <- 0 until imageCount) {
+    val matrix = DenseMatrix.zeros[Int](height, width)
+    for (r <- 0 until height; c <- 0 until width) {
+      matrix(r, c) = imageDataStream.readUnsignedByte()
+    }
+ //   println(matrix)
+    images += DenseVector.tabulate(height * width)({ i => matrix(i / width, i % height) / 255.0 })
+  }
+
+  //println(images.toList)
+  imageDataStream.close
 }
