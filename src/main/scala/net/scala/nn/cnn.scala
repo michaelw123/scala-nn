9829c974a1eb4a1134d2c1d090c0ea8159276cf6
michaelw123 <ok1bridge>
2018-07-10 16:10:19 -0400

    implement cnn

--- src/main/scala/net/scala/nn/cnn.scala
+++ src/main/scala/net/scala/nn/cnn.scala
@@ -4,12 +4,15 @@ import breeze.linalg.{DenseMatrix, DenseVector}
 import breeze.numerics.sigmoid
 import breeze.stats.distributions.Rand
 
+import scala.util.Random
+
 /**
   * Created by Michael Wang on 07/10/2018.
   */
 object cnn {
   type NetworkVector = DenseVector[Double]
   type NetworkMatrix = DenseMatrix[Double]
+  type TrainingSet = List[(NetworkVector, NetworkVector)]
 
   trait Layer {
     val in: Int
@@ -63,11 +66,47 @@ object cnn {
 
   }
 
-  trait CnnNetwork extends network {
+  class CnnNetwork {
+    var layers:List[Layer]=Nil
+    def SGD(trainingData: TrainingSet, epochs: Int, miniBatchSize: Int, eta: Double, testData: Option[TrainingSet]):Unit = {
+      var shuffledTrainingData = trainingData
+
+      for (x <- 0 until epochs) {
+        shuffledTrainingData = Random.shuffle(shuffledTrainingData)
+
+        val miniBatches = shuffledTrainingData.grouped(miniBatchSize).toList
+
+        miniBatches.foreach(b => updateMiniBatch(b, eta, 5.0, trainingData.size))
+
+        val acc = accuracy(shuffledTrainingData)
+
+        testData match {
+          case Some(d) => println(s"Epoch $x: ${evaluate(d)} / ${d.length}")
+          case None => println(s"Epoch $x complete")
+        }
+        println(s"accuracy: ${acc}:/ ${shuffledTrainingData.length}")
+      }
+    }
+    def updateMiniBatch(batch: TrainingSet, eta: Double, lmbta:Double = 0.0, n:Int = 0): Unit = {
+      var newB = layers.map(b => DenseVector.zeros[Double](b.length))
+      var newW = weights.map(w => DenseMatrix.zeros[Double](w.rows, w.cols))
+
+      batch.foreach({
+        case (x, y) => {
+          val (deltaB, deltaW) = backprop(x, y)
+
+          newB = newB.zip(deltaB).map({ case (nb, db) => nb + db })
+          newW = newW.zip(deltaW).map({ case (nw, dw) => nw + dw })
+
+        }
+      })
+      biases = biases.zip(newB).map({ case (b, nb) => b - (eta / batch.length) * nb })
+      weights = weights.zip(newW).map({ case (w, nw) => w - (eta / batch.length) * nw })
+    }
   }
 
   object CnnNetwork {
-    def apply[L <: Layer](layers: Array[L]): network = new network(layers.map(l => l.in).toList)
+    def apply[L <: Layer](layers: Array[L]): CnnNetwork = new CnnNetwork{ layers.map(l => l.in).toList }
   }
 
 }
